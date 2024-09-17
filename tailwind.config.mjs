df37840ec5a2bee37d9054971b8471c56a1db099
Juan Manuel Martínez Ortiz <jmartinez@tricell.com.mx>
2024-09-16 23:21:12 -0600

    first components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,10 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			'primary': '#333',
+			'secondary': '#f3f4f6',
+		},
 	},
 	plugins: [animations],
 }
